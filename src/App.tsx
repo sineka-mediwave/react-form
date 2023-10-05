9036edc04ae5ab2933df29fea386cc3dc65144f7
sineka-mediwave <sineka@mindwaveventures.com>
2023-10-05 10:28:58 +0530

    added app class component

--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,53 @@
-import React, { useState } from "react";
+import React, { Component } from "react";
 import "./App.css";
 import { Button, Input } from "./Components";
 
-function App() {
-  const initialValues = {
+class App extends Component {
+  state = {
     firstName: "",
     secondName: "",
     age: 0,
   };
-  const [input, setInput] = useState(initialValues);
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setInput({ ...input, [e.target.name]: e.target.value });
+  // const [input, setInput] = setState(initialValues);
+  handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    this.setState({
+      ...this.state,
+      [e.target.name]: e.target.value,
+    });
+    // this.setState({ firstName: e.target.value });
   };
-  const display = () => console.log(input);
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <Input
-          name="firstName"
-          type="text"
-          label="Enter Your First Name"
-          onChange={handleChange}
-          placeholder="Enter your First name"
-        />
-        <Input
-          name="secondName"
-          type="text"
-          label="Enter Your Second Name"
-          onChange={handleChange}
-          placeholder="Enter your Second name"
-        />
-        <Input
-          name="age"
-          type="number"
-          label="Enter Your Age"
-          onChange={handleChange}
-          placeholder="Enter your age"
-        />
-        <Button onClick={display} name="Submit" />
-      </header>
-    </div>
-  );
+  display = () => console.log(this.state);
+  render(): React.ReactNode {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <Input
+            name="firstName"
+            type="text"
+            label="Enter Your First Name"
+            onChange={this.handleChange}
+            placeholder="Enter your First name"
+          />
+          <Input
+            name="secondName"
+            type="text"
+            label="Enter Your Second Name"
+            onChange={this.handleChange}
+            placeholder="Enter your Second name"
+          />
+          <Input
+            name="age"
+            type="number"
+            label="Enter Your Age"
+            onChange={this.handleChange}
+            placeholder="Enter your age"
+          />
+          <Button onClick={this.display} name="Submit" />
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
